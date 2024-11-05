5e6fbe660c396dd93cc08fed24aa3eb513d6e812
Zaqueu Silva <58602244+Zqueo@users.noreply.github.com>
2024-11-05 13:26:24 -0300

    Update aula03_float.c

--- aula03_float.c
+++ aula03_float.c
@@ -5,11 +5,28 @@ int main()
     float n1, n2, n3;
     float media;
     
+    char nome[10];
+    
+    scanf("%s", &nome);
+    
     scanf("%f %f %f", &n1, &n2, &n3);
     
     media = (n1+n2+n3)/3.0;
     
-    printf("A média foi %.1f. \n",media);
+    printf("A média do Aluno %s foi %.1f. \n", nome ,media);
+    
+    
+    if (media < 0 || media >10)
+        printf("algum dos valores informados está errado");
+    
+    if(media >= 6){
+        printf("O aluno foi aprovado");
+    } else if (media >=5 && media<6){ 
+        printf("O aluno esta de recuperação");
+    }else {
+        printf("O aluno foi reprovado");
+    }
+    
     
     return 0;
 }
